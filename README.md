2ab03e739bd57b8dc0e9031b56b9e94e4e0f5a00
EdZuVal <john19971008@outlook.com>
2025-01-25 12:15:13 +0300

    Sphinx is used

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Celestorm
+Celestorm
+=========
 
 The package contains a python implementation of the **execution layer** 
 for modular blockchains. This module can be used to create a distributed 
---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Celestorm'
+copyright = '2025, Alexey Poryadin'
+github_username = 'Alesh'
+github_repository = 'https://github.com/Alesh/Celestorm'
+author = 'Alexey Poryadin'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+autodoc_typehints = 'description'
